630f690b0f9e87bfd25bc4b6133271ec05a147bc
emilioCode <emilio_mem@hotmail.com>
2022-06-27 15:49:21 -0400

    Load Fixed.

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,17 +1,15 @@
-export default function Card(props){
-    const col = "col-" + props.col;
-    return(
-        <div className={col}>
-            <br/>
-            <div className="card">
-                <div className="card-body">
-                    <p className="card-text">{props.title}</p>
-                </div>
-                <img src={props.image} alt={props.title}/>
-                <div className="card-body">
-                    <p className="card-text">{props.description}</p>
-                </div>
-            </div>
+export default function Card(props) {
+  const col = "col-" + props.col;
+  return (
+    <div className={col}>
+      <br />
+      <div className="card">
+        <div className="card-body"></div>
+        <img src={props.image} alt={props.title} />
+        <div className="card-body">
+          <h5>{props.title}</h5>
         </div>
-    )
+      </div>
+    </div>
+  );
 }
--- src/pages/Gallery.js
+++ src/pages/Gallery.js
@@ -7,32 +7,45 @@ export default function Gallery() {
   const [apiPath, setApiPath] = useState(
     "https://pokeapi.co/api/v2/pokemon?offset=0&limit=20"
   );
+  const [next, setNext] = useState("");
+
+  const getMore = () => {
+    setApiPath(next);
+  };
 
   useEffect(() => {
     const getPokemons = () => {
-      axios.get(apiPath).then((response) => {
-        let array = response.data.results;
-        let arrayFtm = {};
-        array.forEach((element) => {
-          axios
-            .get(element.url)
-            .then((res) => {
-              arrayFtm = {
-                name: element.name,
-                data: res.data,
-              };
-            })
-            .finally((_) => {
-              let scope = pokemons;
-              scope.push(arrayFtm);
+      //   axios.get(apiPath).then((response) => {
+      //     let array = response.data.results;
+      //     let arrayFtm = {};
+      //     array.forEach((element) => {
+      //       axios
+      //         .get(element.url)
+      //         .then((res) => {
+      //           arrayFtm = {
+      //             name: element.name,
+      //             data: res.data,
+      //           };
+      //         })
+      //         .finally((_) => {
+      //           let scope = pokemons;
+      //           scope.push(arrayFtm);
 
-              setPokemons(scope);
-              console.log(pokemons);
-            });
-          //     debugger
-          //                 console.log(pokemons)
-          // console.log(arrayFtm)
-        });
+      //           setPokemons(scope);
+      //           console.log(pokemons);
+
+      //         });
+      //     });
+      //   });
+
+      axios.get(apiPath).then((resp) => {
+        // debugger;
+        setNext(resp.data.next);
+        for (let i = 0; i < resp.data.results.length; i++) {
+          axios.get(resp.data.results[i].url).then((result) => {
+            setPokemons((prevArray) => [...prevArray, result.data]);
+          });
+        }
       });
     };
 
@@ -45,13 +58,19 @@ export default function Gallery() {
         {pokemons.map((pkm, index) => (
           <Card
             col={3}
-            title={"#" + pkm.data.id + " " + pkm.name}
-            image={pkm.data.sprites.front_default}
-            description={"Pikachú is the 25th Pokemon in the list"}
+            title={"No." + pkm.id + " - " + pkm.name}
+            image={pkm.sprites.front_default}
             key={index}
           />
         ))}
       </div>
+      <br />
+      <div className="text-center">
+        <span className="btn-link" onClick={getMore}>
+          Cargar mas
+        </span>
+      </div>
+      <br />
     </div>
   );
 }
